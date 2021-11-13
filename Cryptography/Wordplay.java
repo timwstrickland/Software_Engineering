38e41eba12882cd0eb82e23bf2982575822a33c9
Timothy Strickland <rcmag13@gmail.com>
2021-11-13 09:39:34 -0500

    Finished the wordplay class
    All methods and test work

--- Cryptography/Wordplay.java
+++ Cryptography/Wordplay.java
@@ -8,6 +8,7 @@
 import java.util.*;
 public class Wordplay {
     public boolean isVowel (char ch) {
+        // This method checks to see if the paramter passed in is a vowel or not.
         boolean vowelCheck = false;
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch =='u') {
             vowelCheck = true;
@@ -19,6 +20,7 @@ public class Wordplay {
     }
     
     public String replaceVowels (String phrase, char ch) {
+        // This method replaces all vowels with the character that is passed in.
         char[] stringToChar = new char[phrase.length()];
         for (int i = 0; i < phrase.length(); i++) {
             if(isVowel(phrase.charAt(i)) == true) {
@@ -32,6 +34,26 @@ public class Wordplay {
         return charString;
     }
     
+    public String emphasize (String phrase, char ch) {
+        // This method only replaces the phrase with the character passed in with a * if its odd, and + if its even.
+        // Depending on the location of the location.
+        char[] stringToChar = new char[phrase.length()];
+        for (int i = 0; i < phrase.length(); i++) {
+            if(phrase.charAt(i) == Character.toUpperCase(ch) || phrase.charAt(i) == Character.toLowerCase(ch))  {
+                if(i % 2 == 0 || i == 0) {
+                    stringToChar[i] = '*';
+                }
+                else {
+                    stringToChar[i] = '+';
+                }
+            }
+            else {
+                stringToChar[i] = phrase.charAt(i);
+            }
+        }
+        String charString = String.valueOf(stringToChar);
+        return charString;
+    }
     
     public void testIsVowel() {
         System.out.println(isVowel('f'));
@@ -48,5 +70,10 @@ public class Wordplay {
         String testString = replaceVowels("Hello World", '*');
         //this should return H*ll* W*rld
         System.out.println(testString);
+    }
+    
+    public void testEmphasize() {
+        System.out.println(emphasize("Mary Bella Abracadabra", 'a'));
+        // Should return "M+ry Bell+ +br*c*d*br+"
     }
 }
