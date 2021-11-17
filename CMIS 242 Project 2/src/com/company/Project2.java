15a8ddca5bcefe407cf445c7ce54c64744506951
Timothy Strickland <rcmag13@gmail.com>
2021-11-16 21:23:31 -0500

    Initial Commit
    4 Classes created, base functionality created. GUI still needed and testing

---
+++ CMIS 242 Project 2/src/com/company/Project2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Project2 {
+
+    public static void main(String[] args) {
+	Electric e = new Electric("Tesla", "Series" , 65000, 3500);
+	System.out.println(e);
+    Hybrid h = new Hybrid("Hyundai", "Accent", 15000, 35);
+    System.out.println(h);
+    }
+}
