774ad2cca4cda81510e7f1327bded0125838bea0
Timothy Strickland <rcmag13@gmail.com>
2021-11-23 10:37:44 -0500

    more practice with GUI
    Updated to better version of a GUI and added an action listener

--- CMIS 242 Project 2/src/com/company/Project2.java
+++ CMIS 242 Project 2/src/com/company/Project2.java
@@ -2,44 +2,53 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Project2 {
+public class Project2 extends Frame implements ActionListener {
+    //Set Variables
+    Button taxButton = new Button("Compute Sales Tax");
+    Button clearButton = new Button("Clear Fields");
+    Button displayReport = new Button("Display Report");
+    Label makeAndModel = new Label("Make and Model");
+    Label salesPrice = new Label("Sales Price");
+    Label milesPerGallon = new Label("Miles per Gallon");
+    Label weightInPounds = new Label("Weight in Pounds");
+    TextField t1 = new TextField ("", 30);
+    TextField t2 = new TextField ("", 30);
+    TextField t3 = new TextField ("", 20);
+    TextField t4 = new TextField ("", 20);
+    TextArea ta1 = new TextArea("", 1, 20);
+
+    public Project2() {
+        super("Automobile Sales Tax Calculator");
+        setLayout(new FlowLayout());
+        add(taxButton);
+        taxButton.addActionListener(this);
+        add(clearButton);
+        add(displayReport);
+        add(makeAndModel);
+        add(salesPrice);
+        add(milesPerGallon);
+        add(weightInPounds);
+        add(t1);
+        add(t2);
+        add(t3);
+        add(t4);
+        add(ta1);
+    }
+
     public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            //create new Frame and label it
-            Frame f = new Frame("Automobile Sales Tax Calculator");
-            //setup default layout
-            f.setLayout(new FlowLayout());
-            //make the frame visible and resizable. Set the size of the frame.
-            f.setVisible(true);
-            f.setResizable(true);
-            f.setSize(300,300);
-            //Create and add buttons
-            Button taxButton = new Button("Compute Sales Tax");
-            Button clearButton = new Button("Clear Fields");
-            Button displayReport = new Button("Display Report");
-            f.add(taxButton);
-            f.add(clearButton);
-            f.add(displayReport);
-            //Create and add labels
-            Label makeAndModel = new Label("Make and Model");
-            Label salesPrice = new Label("Sales Price");
-            Label milesPerGallon = new Label("Miles per Gallon");
-            Label weightInPounds = new Label("Weight in Pounds");
-            f.add(makeAndModel);
-            f.add(salesPrice);
-            f.add(milesPerGallon);
-            f.add(weightInPounds);
-            //Create and add text fields
-            TextField t1, t2, t3, t4;
-            t1 = new TextField ("", 30);
-            t2 = new TextField ("", 30);
-            t3 = new TextField ("", 20);
-            t4 = new TextField ("", 20);
-            f.add(t1);
-            f.add(t2);
-            f.add(t3);
-            f.add(t4);
-        });
+        Project2 p2 = new Project2();
+        p2.setSize(400, 400);
+        p2.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Automobile a = new Automobile();
+        double salesTax = a.salesTax(30000);
+        ta1.setText("" + salesTax);
+
     }
 }
