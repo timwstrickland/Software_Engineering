cf0ca45c9bf948d444c8cf8b8898c2e078abcf81
Timothy Strickland <rcmag13@gmail.com>
2021-12-23 10:47:32 -0500

    Gui design finished
    GUI built, still need to create actionListeners.

--- CMSC 350 Project 3/src/com/company/GuiClass.java
+++ CMSC 350 Project 3/src/com/company/GuiClass.java
@@ -48,11 +48,53 @@ public class GuiClass extends JFrame {
         bottomPanel.setLayout(new GridBagLayout());
         gbc.gridy = 2;
         add(bottomPanel, gbc);
-
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        enterTree = new JLabel("Enter Tree:");
+        topPanel.add(enterTree, gbc);
+        gbc.gridx = 1;
+        enterTreeTextField = new JTextField();
+        enterTreeTextField.setColumns(25);
+        topPanel.add(enterTreeTextField, gbc);
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        makeTree = new JButton("Make Tree");
+        midPanel.add(makeTree, gbc);
+        gbc.gridx = 1;
+        isBalanced = new JButton("Is Balanced?");
+        midPanel.add(makeTree, gbc);
+        gbc.gridx = 2;
+        isFull = new JButton("IsFull?");
+        midPanel.add(isFull, gbc);
+        gbc.gridx = 3;
+        isProper = new JButton("Is Proper?");
+        midPanel.add(isProper, gbc);
+        gbc.gridx= 4;
+        height = new JButton("Height");
+        midPanel.add(height, gbc);
+        gbc.gridx = 5;
+        nodes = new JButton("Nodes");
+        midPanel.add(nodes, gbc);
+        gbc.gridx = 6;
+        inOrder = new JButton("Inorder");
+        midPanel.add(inOrder, gbc);
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        output = new JLabel("Output: ");
+        bottomPanel.add(output, gbc);
+        gbc.gridx = 1;
+        outputTextField = new JTextField();
+        outputTextField.setColumns(25);
+        bottomPanel.add(outputTextField, gbc);
     }
 
     public static void main(String[] args) {
-	// write your code here
+	GuiClass gc = new GuiClass();
+	gc.setSize(550, 250);
+	gc.setVisible(true);
+	gc.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
-
 }
